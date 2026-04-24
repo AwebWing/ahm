17c8c15dbb057e8c6652a82a16a64d4257da2e95
AwebWing <awebghandour@gmail.com>
2026-04-24 17:44:11 +0100

    fix: update base path in vite config to repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets so Github Pages deployment works automatically
+  base: '/ahm/', // Use repository name for GitHub Pages
 })
