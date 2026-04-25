24bf5d24b067b58c3d4869fbc743c200a5e36939
AwebWing <awebghandour@gmail.com>
2026-04-25 02:20:55 +0100

    fix: set base path for Vercel hosting

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ahm/', // Use repository name for GitHub Pages
+  base: '/',
 })
